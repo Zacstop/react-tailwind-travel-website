aa1e21600afe6f9e5a38de4b4077c2f9a6adb73c
zaca3392 <zaca3392@gmail.com>
2022-06-05 11:03:44 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import tw from 'twin.macro';
 import './App.css';
+import { Footer } from './components/footer';
+import { ExploreSection } from './containers/ExploreSection';
+import { TopSection } from './containers/TopSection';
+
+const AppContainer = tw.div`
+  flex
+  flex-col
+  w-full
+  h-full
+  overflow-hidden
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <TopSection/>
+      <ExploreSection/>
+      <Footer/>
+    </AppContainer>
   );
 }
 
